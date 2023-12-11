313e14a646bf5af44b0ea042afc7a2e7e67bc3d6
Aidan Meehan <aodhan66@gmail.com>
2023-12-11 20:57:42 +0000

    update title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Staging</h1>
+        <h1>Vercel CI demo</h1>
       </main>
     </>
   );
